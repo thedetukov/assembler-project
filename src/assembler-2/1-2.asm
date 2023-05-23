ecdd588368572a79d2226ace804e7e9bcc05f4d1
Dmitry Detukov <84131104+thedetukov@users.noreply.github.com>
2023-05-23 15:46:04 +0300

    Added settings file

---
+++ src/assembler-2/1-2.asm
@@ -0,0 +1,21 @@
+.DEVICE ATtiny2313A
+
+; Config PORTB and PORTD for output
+SER r16 ; set 0xff
+OUT $11,r16
+OUT $17,r16
+
+begin:
+
+LDI r30,0xf5
+LDI r31,0xfa
+RCALL set_and_sleep
+
+LDI r30,0xea
+LDI r31,0xfd
+RCALL set_and_sleep
+
+RJMP begin
+
+
+#include "1-common.inc"
