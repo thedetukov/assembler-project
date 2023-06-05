a55d2a663b9f42342c6b28e44e1896783089b9dd
Dmitry Detukov <dmitry.detukov@zxteam.net>
2023-06-05 08:23:49 +0300

    Delete unused functions EEPROM_read and halt

---
+++ src/1-6.asm
@@ -0,0 +1,57 @@
+.DEVICE ATtiny2313A
+
+; Configure PORTB and PORTD for output
+SER r16 ; set 0xff
+OUT $11,r16
+OUT $17,r16
+
+LDI r28, 2
+LDI r29, 125
+PUSH r29
+PUSH r28
+
+begin:
+
+;1
+LDI r30,0xfc
+LDI r31,0xff
+RCALL set_and_sleep
+
+;2
+LDI r30,0xf9
+LDI r31,0xff
+RCALL set_and_sleep
+
+;3
+LDI r30,0xf3
+LDI r31,0xff
+RCALL set_and_sleep
+
+;4
+LDI r30,0xe7
+LDI r31,0xff
+RCALL set_and_sleep
+
+;5
+LDI r30,0xef
+LDI r31,0xfe
+RCALL set_and_sleep
+
+;6
+LDI r30,0xff
+LDI r31,0xfc
+RCALL set_and_sleep
+
+;7
+LDI r30,0xff
+LDI r31,0xf1
+RCALL set_and_sleep
+
+;8
+LDI r30,0xfe
+LDI r31,0xfb
+RCALL set_and_sleep
+
+RJMP begin
+
+#include "1-6-common.inc"
